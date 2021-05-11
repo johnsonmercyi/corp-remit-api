d4e8aa780cc7d025eef52cce913019417ee3a561
Johnson Mercy I <you@example.com>
2021-05-11 14:05:20 +0100

    First Commit

---
+++ src/main/java/com/soft/cr/CorpRemitApplication.java
@@ -0,0 +1,13 @@
+package com.soft.cr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpRemitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpRemitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soft/cr/CorpRemitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.cr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpRemitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
